ef125c55c8542da4940a2ef4f7f7b1f3ee9db005
alec <anhvouw@gmail.com>
2024-06-10 07:27:36 -0400

    fix: show last img

--- player.js
+++ player.js
@@ -1,6 +1,6 @@
 const pic = document.getElementById("pic")
 let counter = 1
 setInterval(() => {
-    pic.setAttribute("src", `./${counter % 4}.jpg`)
+    pic.setAttribute("src", `./${counter % 5}.jpg`)
     counter++
 }, 6000)
